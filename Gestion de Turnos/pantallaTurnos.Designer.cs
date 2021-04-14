907194d9145dfe825102aa87cc2603c91b5af1ea
dn192246 <ricar.arturo@gmail.com>
2021-04-13 19:33:46 -0600

    Modificaciones

--- Gestion de Turnos/pantallaTurnos.Designer.cs
+++ Gestion de Turnos/pantallaTurnos.Designer.cs
@@ -29,57 +29,284 @@ namespace Gestion_de_Turnos
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.btnCerrar = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.c1 = new System.Windows.Forms.Label();
+            this.v1 = new System.Windows.Forms.Label();
+            this.c2 = new System.Windows.Forms.Label();
+            this.v2 = new System.Windows.Forms.Label();
+            this.c3 = new System.Windows.Forms.Label();
+            this.v3 = new System.Windows.Forms.Label();
+            this.c4 = new System.Windows.Forms.Label();
+            this.v4 = new System.Windows.Forms.Label();
+            this.c5 = new System.Windows.Forms.Label();
+            this.v5 = new System.Windows.Forms.Label();
+            this.c6 = new System.Windows.Forms.Label();
+            this.v6 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(50, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(262, 33);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Clientes atendidos";
             // 
             // btnCerrar
             // 
-            this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCerrar.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.btnCerrar.AutoSize = true;
             this.btnCerrar.BackColor = System.Drawing.Color.DarkRed;
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCerrar.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCerrar.Location = new System.Drawing.Point(900, 385);
+            this.btnCerrar.Location = new System.Drawing.Point(900, 383);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Size = new System.Drawing.Size(22, 22);
             this.btnCerrar.TabIndex = 1;
             this.btnCerrar.Text = "X";
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            this.btnCerrar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnCerrar_MouseDown);
+            this.btnCerrar.MouseLeave += new System.EventHandler(this.btnCerrar_MouseLeave);
+            this.btnCerrar.MouseHover += new System.EventHandler(this.btnCerrar_MouseHover);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.v6, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.c6, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.v5, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.c5, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.v4, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.c4, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.v3, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.c3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.v2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.c2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.v1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnCerrar, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.c1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(926, 413);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(177, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 41);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Turno";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(600, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 41);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ventanilla";
+            // 
+            // c1
+            // 
+            this.c1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.c1.AutoSize = true;
+            this.c1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.ForeColor = System.Drawing.Color.Black;
+            this.c1.Location = new System.Drawing.Point(187, 114);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(89, 30);
+            this.c1.TabIndex = 4;
+            this.c1.Text = "Texto1";
+            // 
+            // v1
+            // 
+            this.v1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.v1.AutoSize = true;
+            this.v1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.v1.ForeColor = System.Drawing.Color.Black;
+            this.v1.Location = new System.Drawing.Point(649, 114);
+            this.v1.Name = "v1";
+            this.v1.Size = new System.Drawing.Size(89, 30);
+            this.v1.TabIndex = 5;
+            this.v1.Text = "Texto1";
+            // 
+            // c2
+            // 
+            this.c2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.c2.AutoSize = true;
+            this.c2.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.ForeColor = System.Drawing.Color.Black;
+            this.c2.Location = new System.Drawing.Point(187, 159);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(89, 30);
+            this.c2.TabIndex = 6;
+            this.c2.Text = "Texto1";
+            // 
+            // v2
+            // 
+            this.v2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.v2.AutoSize = true;
+            this.v2.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.v2.ForeColor = System.Drawing.Color.Black;
+            this.v2.Location = new System.Drawing.Point(649, 159);
+            this.v2.Name = "v2";
+            this.v2.Size = new System.Drawing.Size(89, 30);
+            this.v2.TabIndex = 7;
+            this.v2.Text = "Texto1";
+            // 
+            // c3
+            // 
+            this.c3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.c3.AutoSize = true;
+            this.c3.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c3.ForeColor = System.Drawing.Color.Black;
+            this.c3.Location = new System.Drawing.Point(187, 204);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(89, 30);
+            this.c3.TabIndex = 8;
+            this.c3.Text = "Texto1";
+            // 
+            // v3
+            // 
+            this.v3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.v3.AutoSize = true;
+            this.v3.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.v3.ForeColor = System.Drawing.Color.Black;
+            this.v3.Location = new System.Drawing.Point(649, 204);
+            this.v3.Name = "v3";
+            this.v3.Size = new System.Drawing.Size(89, 30);
+            this.v3.TabIndex = 9;
+            this.v3.Text = "Texto1";
+            // 
+            // c4
+            // 
+            this.c4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.c4.AutoSize = true;
+            this.c4.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c4.ForeColor = System.Drawing.Color.Black;
+            this.c4.Location = new System.Drawing.Point(187, 249);
+            this.c4.Name = "c4";
+            this.c4.Size = new System.Drawing.Size(89, 30);
+            this.c4.TabIndex = 10;
+            this.c4.Text = "Texto1";
+            // 
+            // v4
+            // 
+            this.v4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.v4.AutoSize = true;
+            this.v4.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.v4.ForeColor = System.Drawing.Color.Black;
+            this.v4.Location = new System.Drawing.Point(649, 249);
+            this.v4.Name = "v4";
+            this.v4.Size = new System.Drawing.Size(89, 30);
+            this.v4.TabIndex = 11;
+            this.v4.Text = "Texto1";
+            // 
+            // c5
+            // 
+            this.c5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.c5.AutoSize = true;
+            this.c5.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c5.ForeColor = System.Drawing.Color.Black;
+            this.c5.Location = new System.Drawing.Point(187, 294);
+            this.c5.Name = "c5";
+            this.c5.Size = new System.Drawing.Size(89, 30);
+            this.c5.TabIndex = 12;
+            this.c5.Text = "Texto1";
+            // 
+            // v5
+            // 
+            this.v5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.v5.AutoSize = true;
+            this.v5.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.v5.ForeColor = System.Drawing.Color.Black;
+            this.v5.Location = new System.Drawing.Point(649, 294);
+            this.v5.Name = "v5";
+            this.v5.Size = new System.Drawing.Size(89, 30);
+            this.v5.TabIndex = 13;
+            this.v5.Text = "Texto1";
+            // 
+            // c6
+            // 
+            this.c6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.c6.AutoSize = true;
+            this.c6.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c6.ForeColor = System.Drawing.Color.Black;
+            this.c6.Location = new System.Drawing.Point(187, 339);
+            this.c6.Name = "c6";
+            this.c6.Size = new System.Drawing.Size(89, 30);
+            this.c6.TabIndex = 14;
+            this.c6.Text = "Texto1";
+            // 
+            // v6
+            // 
+            this.v6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.v6.AutoSize = true;
+            this.v6.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.v6.ForeColor = System.Drawing.Color.Black;
+            this.v6.Location = new System.Drawing.Point(649, 339);
+            this.v6.Name = "v6";
+            this.v6.Size = new System.Drawing.Size(89, 30);
+            this.v6.TabIndex = 15;
+            this.v6.Text = "Texto1";
             // 
             // pantallaTurnos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.MediumBlue;
+            this.BackColor = System.Drawing.Color.AliceBlue;
             this.ClientSize = new System.Drawing.Size(926, 413);
-            this.Controls.Add(this.btnCerrar);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "pantallaTurnos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Pantalla de turnos";
+            this.Text = "Turnos";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label btnCerrar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label v6;
+        private System.Windows.Forms.Label c6;
+        private System.Windows.Forms.Label v5;
+        private System.Windows.Forms.Label c5;
+        private System.Windows.Forms.Label v4;
+        private System.Windows.Forms.Label c4;
+        private System.Windows.Forms.Label v3;
+        private System.Windows.Forms.Label c3;
+        private System.Windows.Forms.Label v2;
+        private System.Windows.Forms.Label c2;
+        private System.Windows.Forms.Label v1;
+        private System.Windows.Forms.Label c1;
     }
 }
--- Gestion de Turnos/pantallaTurnos.cs
+++ Gestion de Turnos/pantallaTurnos.cs
@@ -25,5 +25,26 @@ namespace Gestion_de_Turnos
                 this.Close();
             }
         }
+
+        private void btnCerrar_MouseHover(object sender, EventArgs e)
+        {
+            btnCerrar.BackColor = Color.Red;
+        }
+
+        private void btnCerrar_MouseLeave(object sender, EventArgs e)
+        {
+            btnCerrar.BackColor = Color.DarkRed;
+            btnCerrar.BorderStyle = BorderStyle.None;
+        }
+
+        private void btnCerrar_MouseClick(object sender, MouseEventArgs e)
+        {
+            btnCerrar.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void btnCerrar_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnCerrar.BorderStyle = BorderStyle.Fixed3D;
+        }
     }
 }
